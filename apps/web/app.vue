2d195dfdd2eddd008db7958575a170dbcfd59247
Sinisa Blagojevic <sinisa.blagojevic@codeus.me>
2024-11-28 13:46:26 +0100

    add test shared package

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <TestFetch />
   </div>
 </template>
---
+++ apps/web/components/TestFetch.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { fetchDummyData } from '@shared'
+
+const data = ref(null);
+
+onMounted(async () => {
+    data.value = await fetchDummyData();
+})
+</script>
+<template>
+    <div v-if="data">
+      <pre>{{ data }}</pre>
+    </div>
+    <div v-else>
+      <p>Loading...</p>
+    </div>
+</template>
