ca2955d627c70cf7cc459538d0b479486a3a41c8
Vahid Kavazović <vahidkavazovic@Vahids-MacBook-Air.local>
2024-12-02 14:47:25 +0100

    Initail builds with splash screen

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Stack } from 'expo-router'
 import defaultConfig from '@tamagui/config/v3'
 import { createTamagui, TamaguiProvider } from 'tamagui'
 import I18nProvider from '@/providers/I18nProvider'
 
+import 'expo-dev-client'
+
+// Create the Tamagui configuration
 const config = createTamagui(defaultConfig)
 
 const RootLayout = () => {
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -4,6 +4,37 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      lightOrange: '#EDEDED',
+      blue: {
+        100: '#009EDC',
+        200: '#0256B9',
+      },
+      orange: {
+        100: '#F78E20',
+        200: '#FCB706',
+      },
+      red: {
+        100: '#F72020',
+      },
+      green: {
+        100: '#13C704',
+        200: '#13C704',
+        300: '#13C70426',
+      },
+      gray: {
+        100: '#999999',
+      },
+    },
+    fontFamily: {
+      rthin: ['Roboto-Thin', 'sans-serif'],
+      rlight: ['Roboto-Light', 'sans-serif'],
+      rregular: ['Roboto-Regular', 'sans-serif'],
+      rmedium: ['Roboto-Medium', 'sans-serif'],
+      ritalic: ['Roboto-Italic', 'sans-serif'],
+      rbold: ['Roboto-Bold', 'sans-serif'],
+      rblack: ['Roboto-Black', 'sans-serif'],
+    },
   },
   plugins: [],
 }
